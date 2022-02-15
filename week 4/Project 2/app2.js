a4c7fc309386e5fb5e8e98b8a6e8ded5defeffc3
shanon-ewingsudds <shannon.ewing-sudds@bryanuniversity.edu>
2022-02-15 12:02:58 -0500

    final week 4 Project

---
+++ week 4/Project 2/app2.js
@@ -0,0 +1,18 @@
+document.getElementById("submit").addEventListener("click", getData);
+
+function getData() {
+    axios.get("https://fedeperin-harry-potter-api-en.herokuapp.com/characters")
+        .then(response => {
+            console.log(response.data)
+            console.log(response.data[0].character);
+            for (let i = 0; i < response.data.length; i++) {
+                const array = response.data[i].character;
+                console.log(array);
+                const div = document.createElement("li");
+                div.textContent = array;
+                div.setAttribute("class", "data_elements")
+                document.body.appendChild(div);
+            };
+        })
+        .catch(error => console.log(error));
+};
